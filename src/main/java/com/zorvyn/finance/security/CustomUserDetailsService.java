8e551e9639e3f138067e5396cbd657966cc9645c
JUNAIDKHAN0112 <mohammadjunaid1604@gmail.com>
2026-04-06 11:46:03 +0530

    added UserDetailsService fix

---
+++ src/main/java/com/zorvyn/finance/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.zorvyn.finance.security;
+
+import com.zorvyn.finance.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(email)
+                .map(user -> User.builder()
+                        .username(user.getEmail())
+                        .password(user.getPassword())
+                        .roles(user.getRole().name()) // ensure role is enum/string
+                        .build()
+                )
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
